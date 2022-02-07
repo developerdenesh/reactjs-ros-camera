418c069c2ccb38bfa3297e570b620d14acc75880
globotix <denesh.siva@globotix.sg>
2022-02-07 12:20:04 +0800

    Adding default topic for use

--- src/rosbridge/echotopoic.js
+++ src/rosbridge/echotopoic.js
@@ -1,58 +1,58 @@
-// EchoTopic.js
-import React, { useState, useEffect } from 'react'
-import { useROS } from 'react-ros'
+    // EchoTopic.js
+    import React, { useState, useEffect } from 'react'
+    import { useROS } from 'react-ros'
 
 
-var listener = null;
-function EchoTopic() {
-    const { createListener, topics } = useROS();
-    const [topic, setTopic] = useState('/');
-    const [someImageURL, setSomeImageURL] = useState('')
+    var listener = null;
+    function EchoTopic() {
+        const { createListener, topics } = useROS();
+        const [topic, setTopic] = useState('/rgbd_FTL/color/image_raw/compressed');
+        const [someImageURL, setSomeImageURL] = useState('')
 
-    useEffect(() => {
-        handleTopic(topic);
-    });
+        useEffect(() => {
+            handleTopic(topic);
+        });
 
-    const unsubscribe = () => {
-        if (listener) {
-            listener.unsubscribe();
-        }
-    }
-
-    const handleTopic = (topicInput) => {
-        if (topic !== topicInput) {
-            setTopic(topicInput);
-            unsubscribe();
-            return;
-        }
-
-        unsubscribe();
-
-        listener = null;
-
-        for (var i in topics) {
-            if (topics[i].path === topicInput) {
-                listener = createListener(topics[i].path,
-                    topics[i].msgType);
-                break;
+        const unsubscribe = () => {
+            if (listener) {
+                listener.unsubscribe();
             }
         }
-        if (listener) {
-            listener.subscribe(handleMsg);
-        } else {
-            console.log("Topic '" + topic + "' not found...make sure to input the full topic path - including the leading '/'");
-        }
-    }
 
-    const handleMsg = (msg) => {
-        setSomeImageURL("data:image/jpeg;base64," + msg.data)
+        const handleTopic = (topicInput) => {
+            if (topic !== topicInput) {
+                setTopic(topicInput);
+                unsubscribe();
+                return;
+            }
+
+            unsubscribe();
+
+            listener = null;
+
+            for (var i in topics) {
+                if (topics[i].path === topicInput) {
+                    listener = createListener(topics[i].path,
+                        topics[i].msgType);
+                    break;
+                }
+            }
+            if (listener) {
+                listener.subscribe(handleMsg);
+            } else {
+                console.log("Topic '" + topic + "' not found...make sure to input the full topic path - including the leading '/', and make sure this is a compressed image topic");
+            }
+        }
+
+        const handleMsg = (msg) => {
+            setSomeImageURL("data:image/jpeg;base64," + msg.data)
+        }
+        
+        return (
+            <div>
+                <b>Topic to echo:  </b><input name="topicInput" defaultValue={topic} onChange={event => handleTopic(event.target.value)} />  <br />
+                <img src={someImageURL}></img>
+            </div>
+        );
     }
-    
-    return (
-        <div>
-            <b>Topic to echo:  </b><input name="topicInput" defaultValue={topic} onChange={event => handleTopic(event.target.value)} />  <br />
-            <img src={someImageURL}></img>
-        </div>
-    );
-}
-export default EchoTopic;
+    export default EchoTopic;
